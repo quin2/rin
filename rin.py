24e75c66b766be43abad6f50162a058127c3aeb4
Quinn Vinlove <quinnvinlove@skylab.org>
2017-12-30 00:37:38 -0500

    added data saving
    Finished saving training data to a file, enabling long term training. Consider data in git to be example, will figure out to hide my own trained matrix.

--- rin.py
+++ rin.py
@@ -1,6 +1,8 @@
 #todo: find out how to make dictionary, 2D list!! or just preload everything smhhhh
 import sys
 import random
+import pickle
+from pathlib import Path
 
 print("hello rin")
 
@@ -58,6 +60,7 @@ emotions = [
 targetLinkages = 3
 
 fileName = "train.rin"
+dataFileName = "data"
 
 #get input character from console
 def getInput():
@@ -67,6 +70,27 @@ def getInput():
         getInput()
 
     return dec
+
+def createArray():
+    toReturn = []
+    for emotion in range(len(emotions)): toReturn +=[[0.0] * len(emotions)]
+    return toReturn
+
+def loadArray():
+    return pickle.load(open(fileName, "rb"))
+
+def saveArray(toSave):
+    pickle.dump(toSave, open(fileName, "wb"))
+    return
+
+def loadData():
+    if not Path(dataFileName).is_file():
+        saveData(0)
+    return pickle.load(open(dataFileName, "rb"))
+
+def saveData(toSave):
+    pickle.dump(toSave, open(dataFileName, "wb"))
+    return
 
 
 #get and load all the input right here
@@ -100,9 +124,22 @@ while len(links) < targetLinkages:
         links.append(rand)
         linkValues.append(0.5)
 
+if not Path(fileName).is_file():
+    saveArray(createArray())
+
+emotionData = loadArray()
+currentNum = loadData() + targetLinkages
+
 #move information to master array
 for idx, item in enumerate(links):
     for item2 in links[idx + 1:]:
-        print(item)
-        print(item2)
-        print()
+        emotionData[item][item2] += linkValues[idx]
+        emotionData[item2][item] += linkValues[idx]
+
+saveArray(emotionData)
+saveData(currentNum)
+
+for n in emotionData:
+    print(n)
+
+print(currentNum)
